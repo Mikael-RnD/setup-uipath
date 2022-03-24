7a142d58b80c19b4a1b378d195810988e42e0be0
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2022-03-24 15:44:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ async function setup() {
     // Get version of tool to be installed
     const version = core.getInput('version');
     console.log(version);
-    
+
     // Download the specific version of the tool, e.g. as a tarball
     const pathToTarball = await tc.downloadTool(getDownloadURL(version));
     console.log(pathToTarball);
 
     // Extract the tarball onto the runner
     const pathToCLI = await tc.extractTar(pathToTarball);
-
+    
+    // Logging!
     console.log(pathToCLI);
 
     // Expose the tool by adding it to the PATH
