11a9556fd3e407c91377d5d0271f7edc5bfa06ea
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2025-06-17 12:14:24 +0200

    revert: update 25.4 default version
    This reverts commit 377be35499d05c857e9f13cdf9f84bb680d39cfd.

--- dist/index.js
+++ dist/index.js
@@ -28618,7 +28618,7 @@ async function getVersion(tool) {
   if (version == '') {
     switch(platformVersion) {
       case '25.4':
-        version = '25.4.9298.26321';
+        version = '25.4.9239.19674';
         break;
       case '24.12':
         version = '24.12.9166.24491';
